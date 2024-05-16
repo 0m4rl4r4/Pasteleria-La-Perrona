fdf4fba7852d180cefee06efd28017c7d836d894
SaidaGutierrez28 <L21540356@delicias.tecnm.mx>
2024-05-15 23:34:12 -0600

    mod saida1

--- vista/vista_recojos.php
+++ vista/vista_recojos.php
@@ -13,7 +13,7 @@ if ($_SESSION['idrol']!=1){
 <!doctype html>
 <html lang="es">
   <head>
-      <title>Entregas | Panadería</title>
+      <title>Entregas | Pasteleria</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
